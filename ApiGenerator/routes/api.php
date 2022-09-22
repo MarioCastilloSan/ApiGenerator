92bf0b7304e15723b39cdc5cc43a805a29c0e08b
fespinoza <felipe.espinoza2019@alu.uct.cl>
2022-09-22 01:06:04 -0300

    fix api

--- ApiGenerator/routes/api.php
+++ ApiGenerator/routes/api.php
@@ -17,17 +17,24 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 //API LOGIN AND REGISTER USER FOR TOKEN
-Route::post('login', 'AuthController@login');
+Route::group([ 'middleware' => 'api'], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
+
 Route::post('register', 'AuthController@register');
 
+/**
+* @API: GET ROUTES [tables,views,routines]
+**/
+Route::group(['middleware' => ['auth:api'], 'prefix' => 'v1'], function(){
+    Route::get('tables', 'tablesController@index');
+    Route::get('views', 'viewController@index');
+    Route::get('routines', 'routinesController@index');
+});
 
-// API GET ROUTES [tables,views,routines]
-
-Route::resource('tables', 'tablesController',
-                ['only' => ['index']]);
-
-Route::resource('routines', 'routinesController',
-                ['only' => ['index']]);
-
-Route::resource('views', 'viewController',
-                ['only' => ['index']]);
